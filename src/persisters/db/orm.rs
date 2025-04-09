15eed680c95d7ff3ae38d982c51d069889c6fc67
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-04-09 21:20:41 +0200

    📦 Moved the 'File' and 'Orm' structs to separate files

---
+++ src/persisters/db/orm.rs
@@ -0,0 +1,190 @@
+//! Contains structures related to a ORM's operations:
+//! - mod [`error`]: error handling for database related problems.
+//! - enum [`Protocol`]: used to distinguish different database protocols.
+//! - struct [`Orm`]: manages database connections and their operations.
+
+use std::fmt;
+use std::ops::Deref;
+
+use super::Sqlite;
+use crate::core::Action;
+use crate::models::{Task, Todo};
+use crate::traits::{DbPersister, Persister};
+
+/// Defines errors related to database management.
+pub mod error {
+    use std::fmt;
+
+    /// Errors related to databases and connection strings.
+    #[derive(Debug)]
+    pub enum Error {
+        /// Used when the provided connection string is not supported.
+        UnsupportedDatabase,
+        /// Used when the provided connection string is incorrect.
+        IncorrectConnectionString,
+    }
+
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::UnsupportedDatabase => {
+                    write!(f, "Unsupported database; defaulting to Sqlite")
+                }
+                Self::IncorrectConnectionString => {
+                    write!(f, "The provided connection string is incorrect")
+                }
+            }
+        }
+    }
+}
+
+/// A database protocol.
+pub enum Protocol {
+    /// An Sqlite database (associated persister: [`Sqlite`]).
+    Sqlite,
+}
+
+impl Protocol {
+    /// Transforms a string slice into a `Protocol` variant.
+    pub fn from(s: &str) -> Self {
+        if s != "sqlite:///" {
+            eprintln!("{}", error::Error::UnsupportedDatabase);
+        }
+        Self::Sqlite
+    }
+
+    /// Returns the `Priority` value as its string representation.
+    pub const fn to_str(&self) -> &str {
+        match self {
+            Self::Sqlite => "sqlite",
+        }
+    }
+}
+
+impl fmt::Display for Protocol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Sqlite => write!(f, "sqlite"),
+        }
+    }
+}
+
+impl Deref for Protocol {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.to_str()
+    }
+}
+
+/// Abstraction of database actions, used to manage a [`Todo`] structure.
+pub struct Orm {
+    /// Database that implements the [`DbPersister`] trait.
+    db: Box<dyn DbPersister>,
+}
+
+impl fmt::Debug for Orm {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Orm")
+            .field("db", &"Box<dyn DbPersister>")
+            .finish()
+    }
+}
+
+impl Orm {
+    /// Constructor of the `Orm` struct, which controls instances of structs
+    /// that implement the [`DbPersister`] trait.
+    pub const fn new(persister: Box<dyn DbPersister>) -> Self {
+        Self { db: persister }
+    }
+
+    /// Creates a `Orm` instance from a connection string.
+    pub fn from(conn: &str) -> Self {
+        Self::new(Self::get_persister(conn))
+    }
+
+    /// Checks if the passed connection string has an Sqlite format.
+    ///
+    /// # Panics
+    /// In case the extension can't be converted to str.
+    pub fn is_sqlite(conn: &str) -> bool {
+        let path = std::path::Path::new(conn);
+
+        conn.eq(":memory:")
+            || path
+                .extension()
+                .is_some_and(|ext| matches!(ext.to_str().unwrap(), "db" | "sqlite3" | "sqlite"))
+    }
+
+    /// Returns a struct that implements the [`DbPersister`] trait based on
+    /// a connection string.
+    pub fn get_persister(conn: &str) -> Box<dyn DbPersister> {
+        let conn = String::from(conn);
+        let mut parts: Vec<&str> = conn.split("://").collect();
+
+        if parts[0].is_empty() {
+            eprintln!("{}", error::Error::IncorrectConnectionString);
+            parts[0] = "tasks.db";
+        }
+
+        let protocol = parts[0];
+
+        if parts.len() == 1 && Self::is_sqlite(protocol) {
+            return Sqlite::from(protocol).boxed();
+        }
+
+        match Protocol::from(protocol) {
+            Protocol::Sqlite => Sqlite::from(&conn).boxed(),
+        }
+    }
+}
+
+impl Persister for Orm {
+    fn boxed(self) -> Box<dyn Persister> {
+        Box::new(self)
+    }
+
+    fn to_string(&self) -> String {
+        self.db.conn()
+    }
+
+    fn exists(&self) -> bool {
+        self.db.exists()
+    }
+
+    fn tasks(&self) -> Vec<Task> {
+        self.db.tasks()
+    }
+
+    fn read(&self) -> Vec<String> {
+        self.db.select()
+    }
+
+    fn edit(&self, ids: &[u32], action: Action) {
+        self.db.update(ids, action);
+    }
+
+    fn save(&self, todo: &Todo) {
+        if self.db.count() == 0 {
+            return self.db.insert(todo);
+        }
+
+        let last = todo.tasks.last().unwrap().to_owned();
+
+        let task = Todo::one(last);
+        self.db.insert(&task);
+    }
+
+    fn replace(&self, todo: &Todo) {
+        self.db.clean();
+        self.db.insert(todo);
+    }
+
+    fn clean(&self) {
+        self.db.clean();
+    }
+
+    fn remove(&self) {
+        self.db.drop_database();
+    }
+}
---
+++ src/persisters/fs/file.rs
@@ -0,0 +1,218 @@
+//! Contains structures related to a file's operations:
+//! - mod [`error`]: error handling for file related problems.
+//! - enum [`Format`]: used to distinguish different file formats.
+//! - struct [`File`]: manages files and their operations.
+
+use std::ffi::OsStr;
+use std::ops::Deref;
+use std::path::PathBuf;
+use std::{fmt, fs};
+
+use super::{Csv, Json, Xml};
+use crate::core::Action;
+use crate::models::{Task, Todo};
+use crate::traits::{FilePersister, Persister};
+
+/// Defines errors related to file management.
+pub mod error {
+    use std::fmt;
+
+    /// Errors related to file and path management.
+    #[derive(Debug)]
+    pub enum Error {
+        /// Used for file format related issues.
+        UnsupportedFormat,
+    }
+
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::UnsupportedFormat => write!(f, "Unsupported file format; defaulting to CSV"),
+            }
+        }
+    }
+}
+
+/// Possible file formats.
+pub enum Format {
+    /// A CSV file (associated persister: [`Csv`]).
+    Csv,
+    /// A JSON file (associated persister: [`Json`]).
+    Json,
+    /// An XML file (associated persister: [`Xml`]).
+    Xml,
+}
+
+impl Format {
+    /// Transforms a string slice into a `Format` variant.
+    pub fn from(s: &str) -> Self {
+        match s {
+            "json" => Self::Json,
+            "csv" => Self::Csv,
+            "xml" => Self::Xml,
+            _ => {
+                eprintln!("{}", error::Error::UnsupportedFormat);
+                Self::Csv
+            }
+        }
+    }
+
+    /// Returns the `Priority` value as its string representation.
+    pub const fn to_str(&self) -> &str {
+        match self {
+            Self::Csv => "csv",
+            Self::Json => "json",
+            Self::Xml => "xml",
+        }
+    }
+}
+
+impl Deref for Format {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.to_str()
+    }
+}
+
+/// Representation of a file that is used to manage a [`Todo`] structure.
+pub struct File {
+    /// File that implements the [`FilePersister`] trait.
+    file: Box<dyn FilePersister>,
+}
+
+impl fmt::Debug for File {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("File")
+            .field("file", &"Box<dyn FilePersister>")
+            .finish()
+    }
+}
+
+impl File {
+    /// Constructor of the `File` struct, which controls instances of structs
+    /// that implement the [`FilePersister`] trait.
+    pub const fn new(persister: Box<dyn FilePersister>) -> Self {
+        Self { file: persister }
+    }
+
+    /// Creates a `File` instance from a path.
+    pub fn from(path: &str) -> Self {
+        let path = Self::check_name(PathBuf::from(path));
+        let file = Self::new(Self::get_persister(path));
+
+        file.check_content();
+
+        file
+    }
+
+    /// Checks the persister's contents. If the persister is empty or its path
+    /// doesn't exists, the persister will get populated by the default contents.
+    ///
+    /// # Panics
+    /// In case the persister can't be populated with the default contents.
+    pub fn check_content(&self) {
+        let path = self.file.path();
+        let is_empty = path.metadata().map_or(true, |meta| meta.len() == 0);
+
+        if path.exists() && !is_empty {
+            return;
+        }
+
+        println!("Creating {}", path.display());
+
+        fs::write(path, self.file.default()).expect("Should have been able to create the file");
+    }
+
+    /// Checks the format of a file and return the same instance with the correct format.
+    pub fn check_name(path: PathBuf) -> PathBuf {
+        let mut path = path;
+
+        let file_name = path
+            .file_name()
+            .unwrap_or(OsStr::new("tasks"))
+            .to_string_lossy()
+            .into_owned();
+
+        let mut file_parts: Vec<&str> = file_name.split('.').collect();
+
+        let new_name = if file_parts[0].is_empty() { "tasks" } else { file_parts[0] };
+        file_parts[0] = new_name;
+
+        file_parts.retain(|part| !part.is_empty() || part == &new_name);
+
+        if file_parts.len() == 1 {
+            file_parts.push("csv");
+        }
+
+        path.set_file_name(file_parts.join("."));
+
+        path.clone()
+    }
+
+    /// Returns a struct that implements the `FilePersister` trait based on the file extension.
+    ///
+    /// # Panics
+    /// In case the file extension can't be converted to `&str`.
+    pub fn get_persister(path: PathBuf) -> Box<dyn FilePersister> {
+        let mut path = path;
+
+        let format = Format::from(path.extension().unwrap().to_str().unwrap());
+
+        path.set_extension(format.to_str());
+
+        match format {
+            Format::Csv => Csv::new(path).boxed(),
+            Format::Json => Json::new(path).boxed(),
+            Format::Xml => Xml::new(path).boxed(),
+        }
+    }
+}
+
+impl Persister for File {
+    fn boxed(self) -> Box<dyn Persister> {
+        Box::new(self)
+    }
+
+    fn to_string(&self) -> String {
+        self.file.path().to_str().unwrap().to_owned()
+    }
+
+    fn exists(&self) -> bool {
+        self.file.exists()
+    }
+
+    fn tasks(&self) -> Vec<Task> {
+        self.file.tasks()
+    }
+
+    fn read(&self) -> Vec<String> {
+        self.file.read()
+    }
+
+    fn edit(&self, _ids: &[u32], _action: Action) {
+        self.file.write(&Todo::from(self));
+    }
+
+    fn save(&self, todo: &Todo) {
+        self.file.write(todo);
+    }
+
+    fn replace(&self, todo: &Todo) {
+        self.file.write(todo);
+    }
+
+    fn clean(&self) {
+        self.file.clean();
+    }
+
+    fn remove(&self) {
+        self.file.remove();
+    }
+}
+
+impl PartialEq for File {
+    fn eq(&self, other: &Self) -> bool {
+        (self.to_string() == other.to_string()) && (self.tasks() == other.tasks())
+    }
+}
