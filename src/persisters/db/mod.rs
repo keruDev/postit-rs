5a14da1e415f732c4e5d7a4369ae6e0cc72cec5f
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-04-22 23:35:37 +0200

    🛠️ Draft of the MongoDB connector

--- src/persisters/db/mod.rs
+++ src/persisters/db/mod.rs
@@ -3,8 +3,10 @@
 //! The currently supported databases are:
 //! - sqlite
 
+mod mongo;
 mod orm;
 mod sqlite;
 
+pub use mongo::Mongo;
 pub use orm::{Orm, Protocol};
 pub use sqlite::Sqlite;
---
+++ src/persisters/db/mongo.rs
@@ -0,0 +1,175 @@
+//! Utilities to handle `Mongo` files.
+//!
+//! The `Mongo` struct implements the [`DbPersister`] trait.
+
+use mongodb::{
+    bson::{doc, Document},
+    sync::{Client, Collection, Database},
+};
+
+use crate::models::{Task, Todo};
+use crate::traits::DbPersister;
+use crate::Action;
+
+/// Representation of a `Mongo` database.
+pub struct Mongo {
+    /// URI used to connect to the `Mongo` database.
+    conn_str: String,
+    /// Connection to the `Mongo` database.
+    connection: Client,
+}
+
+impl Clone for Mongo {
+    fn clone(&self) -> Self {
+        Self {
+            conn_str: self.conn_str.clone(),
+            connection: Client::with_uri_str(&self.conn_str).unwrap(),
+        }
+    }
+}
+
+impl Mongo {
+    /// Creates a `Mongo` instance from a URI.
+    ///
+    /// # Panics
+    /// If the URI can't be converted to str.
+    pub fn from(uri: &str) -> Self {
+        let instance = Self {
+            conn_str: uri.to_string(),
+            connection: Client::with_uri_str(uri).unwrap(),
+        };
+
+        if !instance.exists() {
+            instance.create();
+        }
+
+        instance
+    }
+
+    /// Gets a handle to a database specified by name in the cluster the Client is connected to.
+    pub fn db(&self) -> Database {
+        self.connection.database("test")
+    }
+
+    /// Gets a handle to a collection with type T specified by name of the database.
+    pub fn collection<T: Send + Sync>(&self) -> Collection<T> {
+        self.db().collection::<T>("tasks")
+    }
+}
+
+impl DbPersister for Mongo {
+    fn conn(&self) -> String {
+        self.conn_str.clone()
+    }
+
+    fn boxed(self) -> Box<dyn DbPersister> {
+        Box::new(self)
+    }
+
+    /// Checks if a table exists.
+    ///
+    /// # Panics
+    /// In case the statement can't be prepared.
+    fn exists(&self) -> bool {
+        self.connection
+            .list_database_names()
+            .run()
+            .unwrap()
+            .contains(&"test".to_owned())
+    }
+
+    fn count(&self) -> u32 {
+        if !self.exists() {
+            return 0_u32;
+        }
+
+        self.collection::<u32>()
+            .count_documents(doc! {})
+            .run()
+            .unwrap()
+            .try_into()
+            .unwrap()
+    }
+
+    fn tasks(&self) -> Vec<Task> {
+        self.select().iter().map(|row| Task::from(row)).collect()
+    }
+
+    fn create(&self) {
+        self.db().create_collection("tasks").run().unwrap();
+    }
+
+    fn select(&self) -> Vec<String> {
+        let cursor = self.collection().find(doc! {}).run().unwrap();
+
+        let mut result = Vec::new();
+
+        for doc in cursor {
+            result.push(doc.unwrap());
+        }
+
+        result
+    }
+
+    fn insert(&self, todo: &Todo) {
+        let docs: Vec<Document> = todo
+            .tasks
+            .iter()
+            .map(|task| {
+                doc! {
+                    "id": task.id,
+                    "content": &task.content,
+                    "priority": task.priority.to_str(),
+                    "checked": task.checked,
+                }
+            })
+            .collect();
+
+        self.collection::<Document>()
+            .insert_many(&docs)
+            .run()
+            .unwrap();
+    }
+
+    fn update(&self, todo: &Todo, ids: &[u32], action: Action) {
+        if matches!(action, Action::Drop) {
+            return self.delete(ids);
+        }
+
+        let (field, value) = match action {
+            Action::Check => ("checked", "true"),
+            Action::Uncheck => ("checked", "false"),
+            Action::SetContent => ("content", todo.get(ids)[0].content.as_str()),
+            Action::SetPriority => ("priority", todo.get(ids)[0].priority.to_str()),
+            Action::Drop => unreachable!(),
+        };
+
+        let query = doc! { "$in": ids };
+        let update = doc! { "$set": { field: value } };
+
+        self.collection::<Document>()
+            .update_many(query, update)
+            .run()
+            .unwrap();
+    }
+
+    fn delete(&self, ids: &[u32]) {
+        let docs: Vec<Document> = ids.iter().map(|id| doc! { "id": id }).collect();
+
+        self.collection::<String>()
+            .delete_many(doc! { "$or": docs })
+            .run()
+            .unwrap();
+    }
+
+    fn drop_database(&self) {
+        self.db().drop().run().unwrap();
+    }
+
+    fn clean(&self) {
+        self.collection::<String>()
+            .delete_many(doc! {})
+            .run()
+            .unwrap();
+    }
+}
