4896a12bffcf6a2f96a15bc1684811deece31f82
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-05-18 13:36:40 +0200

    🧹 Changed main.rs so it prints the error instead of returning it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::Parser as _;
-use postit::{Cli, Postit, Result};
+use postit::{Cli, Postit};
 
-fn main() -> Result<()> {
-    Postit::run(Cli::parse())
+fn main() {
+    if let Err(e) = Postit::run(Cli::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
